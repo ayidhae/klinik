0daa6e92e7aacafe398782532e8040184fe76fe0
ElsaTriana <elsasinaga@gmail.com>
2021-03-30 18:05:36 +0700

    pendapatan dan pengeluaran

---
+++ application/views/admin/view_pendapatan.php
@@ -0,0 +1,187 @@
+<body class="app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden">
+  <header class="app-header navbar">
+    <button class="navbar-toggler mobile-sidebar-toggler d-lg-none mr-auto" type="button">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <style type="text/css">
+    body { font-family: sans-serif; }
+    </style>
+    <ul class="nav navbar-nav d-md-down-none">
+      <li class="nav-item px-3">
+        <a class="nav-link" href="<?php echo base_url('/c_user/homeAdmin');?>">Dashboard</a>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav ml-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+          Selamat datang <?php echo $this->session->userdata('username');?>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right">
+          <div class="dropdown-header text-center">
+            <strong>Account</strong>
+          </div>
+          <a class="dropdown-item" href="<?php echo base_url('/c_user/form_update');?>"><i class="fa fa-user"></i> <?php echo $this->session->userdata('username');?></a>
+          <a class="dropdown-item" href="<?php echo base_url('c_user/keluar'); ?>"><i class="fa fa-sign-out"></i> Logout</a>
+        </div>
+      </li>
+    </ul>
+  </header>
+  <div class="app-body">
+    <div class="sidebar">
+      <nav class="sidebar-nav">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo base_url('/c_user/homeAdmin');?>"><i class="icon-speedometer"></i>Admin Dashboard </a>
+          </li>
+          <li class="nav-title">
+            Menu
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href=" <?php echo base_url('/c_user/homeAdmin');?> "><i class="fa fa-home"></i> Home</a>
+          </li>
+        </li>
+          <li class="nav-item">
+            <a class="nav-link" href=" <?php echo base_url('/c_pendapatan/viewPendapatan');?> "><i class="fa fa-home"></i> pendapatan </a>
+          </li>
+          </li>  <!-- pendapatan udh diubah Elsa -->
+  
+        </ul>
+      </nav>
+      <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+    </div>
+    <!-- Main content -->
+    <main class="main">
+      
+      <!-- Breadcrumb -->
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">Home</li>
+        <li class="breadcrumb-item"><a href="#">Admin</a></li>
+        <li class="breadcrumb-item active">Pendapatan</li>
+
+        <!-- Breadcrumb Menu-->
+      </ol>
+      <!-- /.conainer-fluid -->
+     
+      <!-- /.conainer-fluid -->
+      <div class="container-fluid">
+        <div class="card card-accent-success">          
+          <div class="card-header">
+            <h3 class="panel-title pull-left">  Pendapatan </h3>
+            <span title="Tambah Data"><button style="float: right;" class="btn-md btn btn-success"data-toggle="modal" data-target="#myModal">
+            <b>+ Tambah</b>
+          </div>
+          <div class="pull-center">
+          <div class="card-body">
+                <table id="dataPengeluaran" class="table ">
+                <thead>
+                  <tr>
+                    <th>No.</th>
+                    <th>Kode</th>
+                    <th>Tanggal</th>
+                    <th>Keterangan</th>
+                    <th>Jumlah</th>
+                    <th>Aksi</th>
+                  </tr>
+                </thead>
+
+                
+                 <div class="panel-body">
+                            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                            <h4 class="modal-title" id="myModalLabel">Form Tambah Data</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form role="form" method="POST">
+                                                <div class="form-group">
+                                                    <label>Kode</label>
+                                                    <input class="form-control" name="kode" placeholder="Input Kode" />
+                                                </div>
+                                                <div>
+                                                    <label>Keterangan</label>
+                                                    <textarea class="form-control" rows="3" name="ket"></textarea>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label>Tanggal</label>
+                                                    <input class="form-control" type="date" name="tgl" />
+                                                </div>
+                                                <div class="form-group">
+                                                    <label>Jumlah</label>
+                                                    <input class="form-control" type="number" name="jml" />
+                                                </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                                            <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="panel-body">
+                                <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                                <h4 class="modal-title" id="myModalLabel">Form Ubah Data</h4>
+                                            </div>
+                                            <div class="modal-body" id="modal_edit">
+                                                <form role="form" method="POST">
+                                                    <div class="form-group">
+                                                        <label>Kode</label>
+                                                        <input class="form-control" name="kode" placeholder="Input Kode" id="kode" readonly />
+                                                    </div>
+                                                    <div>
+                                                        <label>Keterangan</label>
+                                                        <textarea class="form-control" rows="3" name="ket" id="ket"></textarea>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Tanggal</label>
+                                                        <input class="form-control" type="date" name="tgl" id="tgl" />
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Jumlah</label>
+                                                        <input class="form-control" type="number" name="jml" id="jml" />
+                                                    </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                                                <button type="submit" name="ubah" class="btn btn-primary">Simpan</button>
+                                                </form>
+                <!-- <tbody>
+                  <?php
+                  $customer_user = json_decode(json_encode($pasien), true);                  
+                  $no = 0;
+                  foreach($customer_user as $user):
+                  $no++;
+                  ?>
+                  
+                  <tr>
+                    <td><?php  echo $no; ?></td>
+                    <td><?php  echo $user['No.']; ?></td>
+                    <td><?php echo $user['Kode'];?></td> 
+                       <td><?php echo $user['Keterangan'];?></td>   
+                         <td><?php echo $user['Jumalah'];?></td>     
+                         <td><?php echo $user['aksi'];?></td>                                    
+                  
+                  </tr>
+                  <?php
+                  endforeach;
+                  ?>
+                </tbody> -->
+              </table>
+              
+            </div>
+    </div>
+  </div>
+</div>
+</main>
+</div>
